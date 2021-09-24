a3cffba05c1e6b7636ea25ea4106578f6f1e44e8
Moacir Petry <m.petry9@gmail.com>
2021-09-24 07:57:41 -0300

    test: add user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Add New User' do
+    it 'Create a new user' do
+      expect{ User.create(name: 'User 9000', email: 'user9000@user.com') }.to change(User, :count).by(1)
+    end
+  end
+
+  # context 'Associations' do
+  #   it 'have_many?' do
+  #     is_expected.to have_many(:bookings)
+  #   end
+  # end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/app/channels/'
+  add_filter '/app/controllers/application_controller.rb'
   add_filter '/app/jobs/'
   add_filter '/app/mailers/'
   add_filter '/app/views/'

44a8587e046b2260975b49f11716841797bbda37
Moacir Petry <m.petry9@gmail.com>
2021-09-24 15:19:56 -0300

    test: add request booking test

---
+++ spec/requests/api/v1/booking_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Bookings', type: :request do
+
+  # TODO refactor
+  # describe 'POST /create' do
+  #   it 'returns http success' do
+  #     post booking_path
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe 'DELETE /destroy' do
+    # TODO it needed to be implemented
+    # it 'returns http success' do
+    #   booking = create(:booking)
+    #   delete booking_path(booking)
+    #   expect(response).to have_http_status(:success)
+    # end
+  end
+
+end

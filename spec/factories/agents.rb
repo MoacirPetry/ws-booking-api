0eddbdca982dcf05640376b3bb09c24d0eb8db1d
Moacir Petry <m.petry9@gmail.com>
2021-09-24 15:00:25 -0300

    test: add factories

---
+++ spec/factories/agents.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :agent do
+    name { 'Agent 001' }
+    email { 'agent001@agent.com' }
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { 'User 001' }
+    email { 'user001@user.com' }
+  end
+end

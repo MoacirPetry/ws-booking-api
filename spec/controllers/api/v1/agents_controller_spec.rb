df7554af0456103bf4554c5cdcc866a7395aaf4f
Moacir Petry <m.petry9@gmail.com>
2021-09-24 15:48:56 -0300

    test: add agent json tests

--- spec/controllers/api/v1/agents_controller_spec.rb
+++ spec/controllers/api/v1/agents_controller_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe Api::V1::AgentsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+    it 'render json' do
+      get :index
+      expect(response.header['Content-Type']).to include('application/json')
+    end
+    it 'responds with JSON' do
+      get :index
+      JSON.parse(response.body)
+      expect{response}.to_not raise_error
+    end
   end
 
 end

73219acd509fa7aecb0f75e85a6e83a8fe3173c6
Moacir Petry <m.petry9@gmail.com>
2021-09-24 09:05:34 -0300

    refactor: add has many dependency

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Agent, type: :model do
       expect{ Agent.create(name: 'Agent 9000', email: 'agent9000@user.com') }.to change(Agent, :count).by(1)
     end
   end
+  # TODO needed to test the associations
+  # context 'Associations' do
+  #   it 'have_many?' do
+  #     is_expected.to have_many(:users)
+  #   end
+  # end
 end

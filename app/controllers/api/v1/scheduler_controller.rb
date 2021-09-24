c4570cff387641ecf4121ecc2e8bd4aa49f354b7
Moacir Petry <m.petry9@gmail.com>
2021-09-24 17:41:42 -0300

    refactor: avoid nil class

--- app/controllers/api/v1/scheduler_controller.rb
+++ app/controllers/api/v1/scheduler_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SchedulerController < ApplicationController
   def week
-    week = params[:week].to_datetime
+    week = params[:week].nil? ? Date.today : params[:week].to_datetime
     @bookings = Booking.where(start_at: week.beginning_of_week..week.end_of_week)
     render json: @bookings
   end
